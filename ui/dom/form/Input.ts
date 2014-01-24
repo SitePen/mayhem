16de009c217d82e53959b5927410e9037c360387
Colin Snover <github.com@zetafleet.com>
2014-01-24 23:58:27 +0000

    Add debounce to Input widget and fix cursor resetting position on every input in Chrome. Refs SP#30848

--- ui/dom/form/Input.ts
+++ ui/dom/form/Input.ts
@@ -1,24 +1,48 @@
 import SingleNodeWidget = require('../SingleNodeWidget');
+import util = require('../../../util');
 
 class Input extends SingleNodeWidget {
+	debounceRate:number;
 	firstNode:HTMLInputElement;
 	lastNode:HTMLInputElement;
 	value:string;
+
+	constructor(kwArgs:Object = {}) {
+		this.debounceRate = 100;
+		super(kwArgs);
+	}
+
+	_debounceRateSetter(value:number):void {
+		this.debounceRate = value;
+		this._listen();
+	}
 
 	destroy():void {
 		this.firstNode.oninput = null;
 		super.destroy();
 	}
 
+	_listen():void {
+		if (this.firstNode) {
+			this.firstNode.oninput = util.debounce((event:Event):void => {
+				this.set('value', this.firstNode.value);
+			}, this.debounceRate);
+		}
+	}
+
 	render():void {
 		this.firstNode = this.lastNode = document.createElement('input');
-		this.firstNode.oninput = (event:Event):void => {
-			this.set('value', this.firstNode.value);
-		};
+		this._listen();
 	}
 
 	_valueSetter(value:string):void {
-		this.value = this.firstNode.value = value;
+		this.value = value;
+
+		// Setting the value of the input field causes the cursor to move to the end of the field in at least
+		// Chrome 32, even if the values are identical
+		if (this.firstNode.value !== '' + value) {
+			this.firstNode.value = value;
+		}
 	}
 }
 
