ced4b56c18f97e3a410f4ce11183763f4b0e4e0e
Dean Landolt <dean@deanlandolt.com>
2014-01-31 15:47:03 -0500

    Clean ups for Conditional widget

--- templating/html/ui/Conditional.ts
+++ templating/html/ui/Conditional.ts
@@ -3,80 +3,92 @@ import core = require('../../../interfaces');
 import domUtil = require('../../../ui/dom/util');
 import Processor = require('../../html');
 import Placeholder = require('./Placeholder');
+import util = require('../../../util');
 import widgets = require('../../../ui/interfaces');
 
 class Conditional extends Placeholder {
-	private _clauseWidgets:widgets.IDomWidget[];
-	private _observerMap:IHandle[]; // TODO: clean up
+	alternate:any;
+	private _alternateWidget:widgets.IDomWidget;
+	conditions:any;
+	private _conditionWidgets:widgets.IDomWidget[];
 	private _predicateTerms:Array<string[]>;
 
 	constructor(kwArgs:any) {
 		this._predicateTerms = [];
-		this._clauseWidgets = [];
-		this._observerMap = [];
-
-		var conditions = kwArgs.conditions;
-		delete kwArgs.conditions;
-		var alternate = kwArgs.alternate;
-		delete kwArgs.alternate;
-
+		this._conditionWidgets = [];
+		util.deferSetters(this, [ 'conditions', 'alternate' ], 'render');
 		super(kwArgs);
 
-		var mediator = this.get('mediator');
-
-		// Instantiate all conditional widgets, and alternate, regardless of predicate status
-		if (conditions) {
-			array.forEach(conditions, (condition:any, i:number) => {
-				this._clauseWidgets.push(Processor.widgetFromAst(condition.content, this.app, mediator, this));
-				this._interpretCondition(condition.condition, i);
-			});
-		}
-		if (alternate) {
-			this._clauseWidgets.push(Processor.widgetFromAst(alternate, this.app, mediator, this));
-		}
-
+		this.on('conditionChanged', util.debounce(this._evaluateConditions));
 		this._evaluateConditions();
+	}
+
+	private _alternateSetter(alternate:any):void {
+		this.alternate = alternate;
+		this._alternateWidget = this._constructWidget(alternate);
+	}
+
+	private _conditionsSetter(conditions:any):void {
+		this.conditions = conditions;
+		// TODO clean up old condition widgets if present
+		this._conditionWidgets = [];
+		this._predicateTerms = [];
+
+		var i:number = 0,
+			length:number = conditions.length,
+			condition:any,
+			mediator:core.IMediator = this.get('mediator');
+
+		for (; i < length; ++i) {
+			condition = conditions[i];
+			this._conditionWidgets[i] = this._constructWidget(condition.content);
+			this._predicateTerms[i] = this._interpretCondition(condition.condition);
+		}
+	}
+
+	private _constructWidget(node:any):widgets.IDomWidget {
+		return Processor.widgetFromAst(node, this.app, this.get('mediator'), this);
 	}
 
 	// Evaluate predicate condition and switch currently attached widget if necessary
 	private _evaluateConditions():void {
-		// TODO use debounce in util
-		var i:number,
-			length:number,
+		var i:number = 0,
+			length:number = this._conditionWidgets.length,
 			widget:widgets.IDomWidget,
 			terms:string[],
-			success:boolean,
-			current:Node = this.firstNode;
-		for (i = 0, length = this._clauseWidgets.length; i < length; ++i) {
-			widget = this._clauseWidgets[i];
+			current:widgets.IDomWidget = this.get('content');
+		for (; i < length; ++i) {
+			widget = this._conditionWidgets[i];
 			terms = this._predicateTerms[i];
-			success = terms && eval(terms.join(''));
-			// Evaluate predicate terms if they exist, otherwise its the alternate clause
-			if (success || !terms) {
-				this.content !== widget && this.set('content', widget);
+			// Evaluate predicate terms if they exist and have a widget available
+			if (widget && terms && eval(terms.join(''))) {
+				current !== widget && this.set('content', widget);
 				return;
 			}
 		}
+		// Set content to alterante widget (or nothing at all if no else clause)
+		widget = this._alternateWidget;
+		current !== widget && this.set('content', widget);
 	}
 
-	// Interprets binding in conditional heads and set up watchers
-	private _interpretCondition(condition:any[], predicateIndex:number):void {
-		var mediator:core.IMediator = this.get('mediator');
-		var terms:string[] = [];
-		array.forEach(condition, (term:any, i:number) => {
-			var field:string = term.binding;
-			if (!field) {
-				terms[i] = term.toString();
+	// Interpret binding in conditional heads and create relevant observers
+	private _interpretCondition(condition:any[]):string[] {
+		var mediator:core.IMediator = this.get('mediator'),
+			terms:string[] = [];
+		array.forEach(condition, (item:any, i:number) => {
+			// If not a binding object toString and set in term array
+			if (!item || !item.binding) {
+				terms[i] = item.toString();
 				return;
 			}
-			this._observerMap[field] = mediator.observe(field, (newValue:any) => {
+			// TODO: drip drip
+			mediator.observe(item.binding, (newValue:any) => {
 				terms[i] = JSON.stringify(newValue);
-				this._evaluateConditions();
+				this.emit('conditionChanged');
 			});
-			// TODO: prob not necessary w/ debounce
-			terms[i] = JSON.stringify(mediator.get(field));
+			terms[i] = JSON.stringify(mediator.get(item.binding));
 		});
-		this._predicateTerms[predicateIndex] = terms;
+		return terms;
 	}
 
 }
