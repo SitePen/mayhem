1f1ee8396450b01d93a464e56f3760adb0e4d4b5
Brandon Payton <brandon@happycode.net>
2014-04-06 21:46:31 -0700

    Update ObservableEvented to not refer to DOM Event interface

--- ObservableEvented.ts
+++ ObservableEvented.ts
@@ -4,19 +4,20 @@ import core = require('./interfaces');
 import Evented = require('dojo/Evented');
 import Observable = require('./Observable');
 
+// TODO: Define an Event interface for Mayhem and use it as the type for events in this class
 class ObservableEvented extends Observable implements core.IObservableEvented {
 	constructor(kwArgs?:Object) {
 		super(kwArgs);
 		Evented.apply(this, arguments);
 	}
 
-	emit(type:any, event?:Event):boolean {
+	emit(type:any, event?:any):boolean {
 		return Evented.prototype.emit.apply(this, arguments);
 	}
 
-	on(type:IExtensionEvent, listener:(event:Event) => void):IHandle;
-	on(type:string, listener:(event:Event) => void):IHandle;
-	on(type:any, listener:(event:Event) => void):IHandle {
+	on(type:IExtensionEvent, listener:(event:any) => void):IHandle;
+	on(type:string, listener:(event:any) => void):IHandle;
+	on(type:any, listener:(event:any) => void):IHandle {
 		return Evented.prototype.on.apply(this, arguments);
 	}
 }
