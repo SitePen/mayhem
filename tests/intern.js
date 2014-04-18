ae46917b9e1f27ef6b88c4cac8ea7440a6ea8dd3
Nick Nisi <nick@nisi.org>
2014-04-18 17:02:37 -0500

    upgrade intern and update intern config
    * upgrade to intern 1.6.2
    * correct baseUrl property

--- tests/intern.js
+++ tests/intern.js
@@ -44,7 +44,7 @@ define({
 	// Configuration options for the module loader; any AMD configuration options supported by the Dojo loader can be
 	// used here
 	loader: {
-		baseUrl: '../',
+		baseUrl: typeof process !== 'undefined' ? '../' : '../../../',
 		// Packages that should be registered with the loader in each testing environment
 		packages: [
 			{ name: 'mayhem', location: 'framework' },
