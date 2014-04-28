1ce83c830a1dc8e192cf84c6c905958dc4e0c977
Nick Nisi <nick@nisi.org>
2014-04-28 16:26:12 -0500

    fix Label tests

--- tests/ui/dom/form/Label.ts
+++ tests/ui/dom/form/Label.ts
@@ -3,14 +3,22 @@
 import assert = require('intern/chai!assert');
 import registerSuite = require('intern!object');
 import LabelRenderer = require('../../../../ui/dom/form/Label');
-import Observable = require('../../../../Observable');
+import Widget = require('../../../../ui/Widget');
 
 registerSuite({
 	name: 'ui/dom/form/Label',
 
+	setup() {
+		Widget.prototype._renderer = new LabelRenderer();
+	},
+
+	teardown() {
+		Widget.prototype._renderer = undefined;
+	},
+
 	render() {
 		var renderer = new LabelRenderer(),
-			widget:any = new Observable(),
+			widget:any = new Widget(),
 			attribute:any;
 
 		renderer.render(widget);
