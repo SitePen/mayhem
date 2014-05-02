fa86e10bb11c5c6e965ee7a22b1e16081ad7bf9e
Nick Nisi <nick@nisi.org>
2014-05-02 16:33:25 -0500

    add more Iterator tests

--- tests/ui/dom/Iterator.ts
+++ tests/ui/dom/Iterator.ts
@@ -19,24 +19,54 @@ var iterator:any,
 registerSuite({
 	name: 'ui/dom/Iterator',
 
-	beforeEach() {
-		iterator = new Iterator()
+	beforeEach():void {
+		iterator = new Iterator();
 		renderer = iterator._renderer;
 	},
 
-	afterEach() {
+	afterEach():void {
 		iterator = util.destroy(iterator);
 	},
 
-	'#destroy'() {
+	'#initialize': function ():void {
+		var calls:any = {},
+			destroyCalled:boolean = false,
+			selectedItemCalled:boolean = false;
+
+		iterator._impl = {
+			set: function (key:string, value:any):any {
+				calls[key] = true;
+			},
+			destroy: function ():void {
+				destroyCalled = true;
+			}
+		};
+
+		iterator.on('selection', function ():void {
+			selectedItemCalled = true;
+		});
+
+		renderer.initialize(iterator);
+		iterator.set('selectedItem', null);
+		iterator.set('allowSelectAll', true);
+		iterator.set('allowTextSelection', true);
+		iterator.set('source', null);
+
+		assert.isTrue(selectedItemCalled);
+		assert.property(calls, 'allowSelectAll');
+		assert.property(calls, 'allowTextSelection');
+		assert.isTrue(destroyCalled);
+	},
+
+	'#destroy'():void {
 		var destroyed = false,
 			widget:any = {
-				_impl: { destroy() { destroyed = true; } }
-			}
-		assert.doesNotThrow(function () {
+				_impl: { destroy():void { destroyed = true; } }
+			};
+		assert.doesNotThrow(function ():void {
 			renderer.destroy(widget);
 		}, 'Destroying renderer should not throw');
 		assert.isTrue(destroyed, 'Widget should have been destroyed');
 		assert.isNull(widget._impl, 'Widget list implementation should be null');
-	},
+	}
 });
