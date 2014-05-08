e6c3ee6ff6481e619425dd934b3d30350c08ef05
devpaul <pshannon@sitepen.com>
2014-05-08 16:36:44 -0700

    Fix unit tests for NodeTargetProxty to properly target browsers only for document tests

--- tests/binding/proxties/NodeTargetProxty.ts
+++ tests/binding/proxties/NodeTargetProxty.ts
@@ -4,91 +4,95 @@ import assert = require('intern/chai!assert');
 import binding = require('../../../binding/interfaces');
 import NodeTargetProxty = require('../../../binding/proxties/NodeTargetProxty');
 import MockProxty = require('../support/MockProxty');
-import registerSuite = require('intern!object');
+import bdd = require('intern!bdd');
+import has = require('dojo/has');
 
-registerSuite({
-	name: 'binding/proxties/NodeTargetProxty',
+bdd.describe('binding/proxties/NodeTargetProxty', function () {
+    bdd.describe('.test', function () {
+        if(has('host-browser')) {
+            bdd.it('should be able to bind a node', function () {
+                var element = document.createElement('div'),
+                    result = NodeTargetProxty.test({
+                        object: element,
+                        binding: '',
+                        binder: null
+                    });
 
-	'.test': function () {
-		var element = document.createElement('div'),
-			result:boolean = NodeTargetProxty.test({
-				object: element,
-				binding: '',
-				binder: null
-			});
+                assert.isTrue(result, 'Should be able to bind a node');
+            });
+        }
+        bdd.it('should not be able to bind a non-node', function () {
+            var result = NodeTargetProxty.test({
+                object: {},
+                binding: '',
+                binder: null
+            });
 
-		assert.isTrue(result, 'Should be able to bind a node');
+            assert.isFalse(result, 'Should not be able to bind a non-node');
+        });
+    });
 
-		result = NodeTargetProxty.test({
-			object: {},
-			binding: '',
-			binder: null
-		});
+    if(has('host-browser')) {
+        bdd.describe('basic tests', function () {
+            var sourceObject = document.createElement('div'), source, target;
 
-		assert.isFalse(result, 'Should not be able to bind a non-node');
-	},
+            sourceObject.id = '1';
 
-	'basic tests': function () {
-		var sourceObject = document.createElement('div'),
-			source:NodeTargetProxty<string>,
-			target:MockProxty<string>;
+            source = new NodeTargetProxty({
+                object: sourceObject,
+                binding: 'id',
+                binder: null
+            });
 
-		sourceObject.id = '1';
+            target = new MockProxty({
+                object: {},
+                binding: 'foo',
+                binder: null
+            });
 
-		source = new NodeTargetProxty<string>({
-			object: sourceObject,
-			binding: 'id',
-			binder: null
-		});
+            assert.strictEqual(source.get(), sourceObject.id, 'Bound source property should match value of source property object');
 
-		target = new MockProxty<string>({
-			object: {},
-			binding: 'foo',
-			binder: null
-		});
+            var handle = source.bindTo(target);
+            assert.strictEqual(target.get(), source.get(), 'Target value should match bound source value');
 
-		assert.strictEqual(source.get(), sourceObject.id, 'Bound source property should match value of source property object');
+            sourceObject.id = '2';
+            assert.strictEqual(source.get(), '2', 'Bound source property should show current value of source');
 
-		var handle = source.bindTo(target);
-		assert.strictEqual(target.get(), source.get(), 'Target value should match bound source value');
+            source.set('4');
+            assert.strictEqual(sourceObject.id, '4', 'Bound source property should change when source is updated');
 
-		sourceObject.id = '2';
-		assert.strictEqual(source.get(), '2', 'Bound source property should show current value of source');
+            handle.remove();
+            sourceObject.id = '5';
+            assert.strictEqual(source.get(), '5', 'Bound source property should match value of source property object even when target is removed');
 
-		source.set('4');
-		assert.strictEqual(sourceObject.id, '4', 'Bound source property should change when source is updated');
+            assert.doesNotThrow(function () {
+                handle.remove();
+            }, 'Removing handle a second time should be a no-op');
 
-		handle.remove();
-		sourceObject.id = '5';
-		assert.strictEqual(source.get(), '5', 'Bound source property should match value of source property object even when target is removed');
+            sourceObject.id = '6';
+            handle = source.bindTo(target);
+            assert.strictEqual(target.get(), '6', 'Re-binding to target should reset target value');
+            handle.remove();
 
-		assert.doesNotThrow(function () {
-			handle.remove();
-		}, 'Removing handle a second time should be a no-op');
+            sourceObject.id = '7';
+            source.bindTo(target, { setValue: false });
+            assert.strictEqual(target.get(), '6', 'Re-binding to target with setValue=false should not reset target value');
 
-		sourceObject.id = '6';
-		handle = source.bindTo(target);
-		assert.strictEqual(target.get(), '6', 'Re-binding to target should reset target value');
-		handle.remove();
+            source.bindTo(null);
+            sourceObject.id = '8';
+            assert.strictEqual(target.get(), '6', 'Removing binding should stop old target property from updating');
 
-		sourceObject.id = '7';
-		source.bindTo(target, { setValue: false });
-		assert.strictEqual(target.get(), '6', 'Re-binding to target with setValue=false should not reset target value');
+            source.destroy();
+            sourceObject.id = '7';
+            assert.isUndefined(source.get(), 'Destroyed source property should no longer have a value from the source property object');
 
-		source.bindTo(null);
-		sourceObject.id = '8';
-		assert.strictEqual(target.get(), '6', 'Removing binding should stop old target property from updating');
+            assert.doesNotThrow(function () {
+                source.destroy();
+            }, 'Destroying property a second time should be a no-op');
 
-		source.destroy();
-		sourceObject.id = '7';
-		assert.isUndefined(source.get(), 'Destroyed source property should no longer have a value from the source property object');
-
-		assert.doesNotThrow(function () {
-			source.destroy();
-		}, 'Destroying property a second time should be a no-op');
-
-		assert.doesNotThrow(function () {
-			source.set('8');
-		}, 'Setting the value of a destroyed proxty should be a no-op');
-	}
+            assert.doesNotThrow(function () {
+                source.set('8');
+            }, 'Setting the value of a destroyed proxty should be a no-op');
+        });
+    }
 });
