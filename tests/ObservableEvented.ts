3eb083636c814c6e1bde19429eb9f65035220c5a
James Morrin <jmorrin@sitepen.com>
2014-05-08 16:02:25 -0400

    increase coverage for ObservableEvented

--- tests/ObservableEvented.ts
+++ tests/ObservableEvented.ts
@@ -12,6 +12,72 @@ class TestEvent extends Event {
 
 registerSuite({
 	name: 'ObservableEvented',
+	
+	'#on and #emit event type transform' () {
+		var observableEvented = new ObservableEvented({
+				'ontest': 'newEvent'
+			}),
+			emittedEvent:any,
+			listenerCallCount = 0;
+		
+		observableEvented.on('newEvent', function (actualEvent:TestEvent) {
+			listenerCallCount++;
+			assert.strictEqual(actualEvent.sourceEvent.value, emittedEvent.value);
+		});
+		
+
+		emittedEvent = new TestEvent({ type: 'test', value: 'foo'});
+		observableEvented.emit(emittedEvent);
+		assert.strictEqual(listenerCallCount, 1);
+
+		emittedEvent = new TestEvent({ type: 'test', value: 'bar'});
+		observableEvented.emit(emittedEvent);
+		assert.strictEqual(listenerCallCount, 2);
+	
+	},
+
+	'#on and #emit function method' () {
+		var observableEvented = new ObservableEvented({
+				'ontest': function (actualEvent:TestEvent) {
+					listenerCallCount++;
+					assert.strictEqual(actualEvent.value, emittedEvent.value);
+				}	
+			}),
+			emittedEvent:any,
+			listenerCallCount = 0;
+
+		emittedEvent = new TestEvent({ type: 'test', value: 'foo'});
+		observableEvented.emit(emittedEvent);
+		assert.strictEqual(listenerCallCount, 1);
+
+		emittedEvent = new TestEvent({ type: 'test', value: 'bar'});
+		observableEvented.emit(emittedEvent);
+		assert.strictEqual(listenerCallCount, 2);
+	
+	},
+
+	'#on and #emit bubbles' () {
+		var observableEvented = new ObservableEvented(),
+			parentEvented = new ObservableEvented(),
+			emittedEvent:any,
+			listenerCallCount = 0;
+
+		parentEvented.on('test', function (actualEvent:TestEvent) {
+			listenerCallCount++;
+			assert.strictEqual(actualEvent.value, emittedEvent.value);
+		});
+
+		observableEvented.set('parent', parentEvented);
+
+		emittedEvent = new TestEvent({ type: 'test', value: 'foo', bubbles: true });
+		observableEvented.emit(emittedEvent);
+		assert.strictEqual(listenerCallCount, 1);
+
+		emittedEvent = new TestEvent({ type: 'test', value: 'bar', bubbles: true });
+		observableEvented.emit(emittedEvent);
+		assert.strictEqual(listenerCallCount, 2);
+	
+	},
 
 	'#on and #emit' () {
 		var observableEvented = new ObservableEvented(),
