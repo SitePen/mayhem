6a759245664f63d59412dd93d977626aa05f955e
Sethen Maleno <smaleno@sitepen.com>
2014-10-14 11:44:19 -0400

    Fixed pathing issue in Observable test, added to the ErrorHandler test.

--- tests/unit/core/ErrorHandler.ts
+++ tests/unit/core/ErrorHandler.ts
@@ -1,15 +1,24 @@
 /// <reference path="../../intern" />
 import assert = require('intern/chai!assert');
+import Application = require('../../../Application');
 import ErrorHandler = require('../../../ErrorHandler');
 import Observable = require('../../../Observable');
 import registerSuite = require('intern!object');
 
-class MockApplication extends Observable {
-    _ui:any;
-    view: any;
+declare var application;
+declare var errorHandler;
+
+interface IMockUi {
+    _view:any;
 }
 
-declare var errorHandler;
+class MockUi extends Observable implements IMockUi {
+    _view:any;
+    constructor() {
+        super();
+        this._view = 'ui';
+    }
+}
 
 registerSuite({
     name: 'mayhem/ErrorHandler',
@@ -17,9 +26,18 @@ registerSuite({
         errorHandler.destroy();
     },
 
+    before() {
+        application = new Application({
+            components: {
+                ui: new MockUi()
+            }
+        });
+        application.startup();
+    },
+
     beforeEach() {
         errorHandler = new ErrorHandler({
-            app: new MockApplication()
+            app: application
         });
         errorHandler.startup();
     },
@@ -33,7 +51,9 @@ registerSuite({
     },
 
     'assert handling of errors in DOM'() {
-        // ...
+        setTimeout(function () {
+            throw new Error();
+        }, 0);
     },
 
     'assert handling of errors in Node'() {
