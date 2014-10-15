1049d869046933d002b3aa24784b1ab2571a3699
Sethen Maleno <smaleno@sitepen.com>
2014-10-15 15:24:59 -0400

    Added to ErrorHandler tests.

--- ErrorHandler.ts
+++ ErrorHandler.ts
@@ -9,61 +9,65 @@ import View = require('./ui/View');
 import WebApplication = require('./WebApplication');
 
 class ErrorHandler extends Observable {
-    private _app:WebApplication;
-    private _handleGlobalErrors:boolean;
-    private _handle:IHandle;
+	private _app:WebApplication;
+	private _handleGlobalErrors:boolean;
+	private _handle:IHandle;
 
-    get:ErrorHandler.Getters;
-    set:ErrorHandler.Setters;
+	get:ErrorHandler.Getters;
+	set:ErrorHandler.Setters;
 
-    _initialize():void {
-        super._initialize();
-        this._handleGlobalErrors = true;
-    }
+	_initialize():void {
+		super._initialize();
+		this._handleGlobalErrors = true;
+	}
 
-    destroy():void {
-        this._handle && this._handle.remove();
-        super.destroy();
-    }
+	destroy():void {
+		this._handle && this._handle.remove();
+		super.destroy();
+	}
 
-    handleDomError(error:Error):void {
-        var ErrorView:typeof View = <any> require('./templating/html!./views/Error.html');
-        var view = new ErrorView({
-            app: this._app,
-            model: error
-        });
-        this._app.get('ui').set('view', view);
-    }
+	handleDomError(error:any):void {
+		if (typeof error === 'string') {
+			error = new Error(error);
+		}
 
-    handleNodeError(error:string):void {
-        this._app.log(error);
-    }
+		var ErrorView:typeof View = <any> require('./templating/html!./views/Error.html');
+		var view = new ErrorView({
+			app: this._app,
+			model: error
+		});
+		this._app.get('ui').set('view', view);
+	}
 
-    startup():void {
-        var self = this;
-        if (this._handleGlobalErrors) {
-            if (has('host-browser')) {
-                this._handle = aspect.before(window, 'onerror', function (error:Error):void {
-                    self.handleDomError(error);
-                });
-            }
-            else if (has('host-node')) {
-                process.on('uncaughtException', function (error:Error):void {
-                    self.handleNodeError(error.name);
-                });
-            }
-        }
-    }
+	handleNodeError(error:string):void {
+		this._app.log(error);
+	}
+
+	startup():void {
+		var self = this;
+		if (this._handleGlobalErrors) {
+			if (has('host-browser')) {
+				this._handle = aspect.before(window, 'onerror', function (error:Error):void {
+					self.handleDomError(error);
+				});
+			}
+			else if (has('host-node')) {
+				process.on('uncaughtException', function (error:Error):void {
+					self.handleNodeError(error.name);
+				});
+			}
+		}
+	}
 }
 
 module ErrorHandler {
-    export interface Getters extends Observable.Getters {
-        (key:'handleGlobalErrors'):boolean;
-    }
+	export interface Getters extends Observable.Getters {
+		(key:'handleGlobalErrors'):boolean;
+	}
 
-    export interface Setters extends Observable.Setters {
-        (key:'handleGlobalErrors', value:boolean):void;
-    }
+	export interface Setters extends Observable.Setters {
+		(key:'handleGlobalErrors', value:boolean):void;
+	}
 }
 
 export = ErrorHandler;
--- tests/unit/core/ErrorHandler.ts
+++ tests/unit/core/ErrorHandler.ts
@@ -5,58 +5,79 @@ import ErrorHandler = require('../../../ErrorHandler');
 import Observable = require('../../../Observable');
 import registerSuite = require('intern!object');
 
-declare var application;
-declare var errorHandler;
+var application;
+var errorHandler;
+var defaultView:string = 'currentView';
+var errorMessage:string = 'Mayhem Error Message';
+var ui:any;
 
 interface IMockUi {
-    _view:any;
+	_view:any;
 }
 
 class MockUi extends Observable implements IMockUi {
-    _view:any;
-    constructor() {
-        super();
-        this._view = 'ui';
-    }
+	_view:any;
+	constructor() {
+		super();
+	}
 }
 
 registerSuite({
-    name: 'mayhem/ErrorHandler',
-    afterEach() {
-        errorHandler.destroy();
-    },
+	name: 'mayhem/ErrorHandler',
+	afterEach() {
+		errorHandler.destroy();
+	},
 
-    before() {
-        application = new Application({
-            components: {
-                ui: new MockUi()
-            }
-        });
-        application.startup();
-    },
+	before() {
+		application = new Application({
+			components: {
+				ui: {
+					constructor: MockUi
+				}
+			}
+		});
+		return application.startup();
+	},
 
-    beforeEach() {
-        errorHandler = new ErrorHandler({
-            app: application
-        });
-        errorHandler.startup();
-    },
+	beforeEach() {
+		errorHandler = new ErrorHandler({
+			app: application
+		});
+		ui = errorHandler._app.get('ui');
+		ui.set('view', defaultView);
+		return errorHandler.startup();
+	},
 
-    'assert default properties'() {
-        assert.isTrue(errorHandler._handleGlobalErrors);
-    },
+	'assert default properties'() {
+		assert.isTrue(errorHandler._handleGlobalErrors);
+		assert.strictEqual(ui._view, defaultView, 'ui view is equal to default view');
+		assert.isUndefined(ui.get('view').model, 'model is undefined');
+	},
 
-    'assert startup handles errors'() {
+	'assert startup handles errors'() {
+		var dfd = this.async(1000);
 
-    },
+		window.addEventListener('error', function (event) {
+			event.preventDefault();
+		}, false);
 
-    'assert handling of errors in DOM'() {
-        setTimeout(function () {
-            throw new Error();
-        }, 0);
-    },
+		setTimeout(function () {
+			throw new Error(errorMessage);
+		}, 0);
 
-    'assert handling of errors in Node'() {
-        // ...
-    }
+		ui.observe('view', dfd.callback(function (newValue) {
+			assert.isObject(newValue);
+			assert.include(ui.get('view').get('model').message, errorMessage, 'includes error message');
+		}));
+	},
+
+	'assert handling of errors in DOM'() {
+		errorHandler.handleDomError(new Error(errorMessage));
+		assert.isDefined(ui.get('view').get('model'), 'model is defined');
+		assert.strictEqual(ui.get('view').get('model').message, errorMessage, 'error message is equal to default');
+	},
+
+	'assert handling of errors in Node'() {
+		// ...
+	}
 });
