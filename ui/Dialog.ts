be77466cd0daee807798b735ae0f6510ae9b78e6
Bryan Forbes <bryan@reigndropsfall.net>
2014-11-14 18:09:01 -0600

    Added initial dialog (WIP)

---
+++ ui/Dialog.ts
@@ -0,0 +1,33 @@
+/// <amd-dependency path="./dom/Dialog" />
+
+import Container = require('./Container');
+import has = require('../has');
+
+interface Dialog extends Container {
+	get:Dialog.Getters;
+	on:Dialog.Events;
+	set:Dialog.Setters;
+}
+
+module Dialog {
+	export interface Events extends Container.Events {}
+	export interface Getters extends Container.Getters {
+		(key:'isShowing'):boolean;
+		(key:'title'):string;
+	}
+	export interface Setters extends Container.Setters {
+		(key:'isShowing', value:boolean):void;
+		(key:'title', value:string):void;
+	}
+}
+
+var Dialog:{
+	new (kwArgs:HashMap<any>):Dialog;
+	prototype:Dialog;
+};
+
+if (has('host-browser')) {
+	Dialog = <typeof Dialog> require('./dom/Dialog');
+}
+
+export = Dialog;
