704338c809c683111eebb6a4de4edc1f4c66d929
Mangala SSS Khalsa <mkhalsa@sitepen.com>
2014-12-01 21:02:24 -0700

    Application: add more unit tests

--- tests/unit/core/Application.ts
+++ tests/unit/core/Application.ts
@@ -1,7 +1,10 @@
+/// <reference path="../../../dojo" />
 /// <reference path="../../intern" />
 
 import Application = require('../../../Application');
+import aspect = require('dojo/aspect');
 import assert = require('intern/chai!assert');
+import LogLevel = require('../../../LogLevel');
 import registerSuite = require('intern!object');
 
 var app:Application;
@@ -14,11 +17,7 @@ registerSuite({
 	},
 
 	'#run'() {
-		app = new Application({
-			components: {
-				errorHandler: null
-			}
-		});
+		app = new Application();
 		var runPromise = app.run();
 
 		return runPromise.then(function () {
@@ -26,26 +25,134 @@ registerSuite({
 		});
 	},
 
-	'#log'() {
-		var testMessage = 'test message';
-		var logCalled = false;
+	'#handleError': {
+		'with errorHandler'() {
+			var receivedError:Error;
 
-		app = new Application({
-			logger: {
-				log: function (message:string, level:number, category:string):void {
-					logCalled = true;
-					assert.strictEqual(message, testMessage)
+			app = new Application({
+				errorHandler: {
+					handleError(error:Error) {
+						receivedError = error;
+					}
+				},
+				components: {
+					errorHandler: null
 				}
-			},
-			components: {
-				errorHandler: null,
-				logger: null
-			}
-		});
+			});
 
-		return app.run().then(function () {
-			app.log(testMessage);
-			assert.isTrue(logCalled, 'logger.log should be called');
-		});
+			return app.run().then(function () {
+				var testError = new Error('test');
+
+				app.handleError(testError);
+				assert.strictEqual(receivedError, testError, 'error handler should receive passed Error object');
+
+				// TODO: remove this cleanup when Application is fixed
+				// https://github.com/SitePen/mayhem/issues/14
+				var defaultConfig = Application._defaultConfig;
+				// TS7017
+				(<any> defaultConfig)['errorHandler'] = null;
+			});
+		},
+
+		'without errorHandler'() {
+			var loggedLevel:number;
+
+			// TODO: remove this cleanup when Application is fixed
+			// https://github.com/SitePen/mayhem/issues/14
+			var defaultConfig = Application._defaultConfig;
+			// TS7017
+			(<any> defaultConfig)['errorHandler'] = null;
+
+			app = new Application({
+				logger: {
+					log(message:string, level:number, category:string):void {
+						loggedLevel = level;
+					}
+				},
+				components: {
+					errorHandler: null,
+					logger: null
+				}
+			});
+
+			return app.run().then(function () {
+				app.handleError(new Error('test error'));
+				assert.strictEqual(loggedLevel, LogLevel.ERROR, 'logger.log should be called with correct LogLevel');
+
+				// TODO: remove this cleanup when Application is fixed
+				// https://github.com/SitePen/mayhem/issues/14
+				var defaultConfig = Application._defaultConfig;
+				// TS7017
+				(<any> defaultConfig)['logger'] = null;
+			});
+		}
+	},
+
+	'#log': {
+		'with logger'() {
+			var receivedArguments:any[];
+
+			app = new Application({
+				logger: {
+					log(message:string, level:number, category:string):void {
+						receivedArguments = Array.prototype.slice.call(arguments);
+					}
+				},
+				components: {
+					errorHandler: null,
+					logger: null
+				}
+			});
+
+			return app.run().then(function () {
+				var logArguments = [ 'test message', LogLevel.LOG, 'test category'];
+
+				app.log.apply(app, logArguments);
+				assert.deepEqual(receivedArguments, logArguments, 'logger.log should be called with correct arguments');
+
+				// TODO: remove this cleanup when Application is fixed
+				// https://github.com/SitePen/mayhem/issues/14
+				var defaultConfig = Application._defaultConfig;
+				// TS7017
+				(<any> defaultConfig)['logger'] = null;
+			});
+		},
+
+		'without logger'() {
+			// TODO: remove this cleanup when Application is fixed
+			// https://github.com/SitePen/mayhem/issues/14
+			var defaultConfig = Application._defaultConfig;
+			// TS7017
+			(<any> defaultConfig)['logger'] = null;
+
+			app = new Application({
+				components: {
+					logger: null
+				}
+			});
+
+			return app.run().then(function () {
+				var testMessage = 'test message';
+				var logMessage:string;
+				var handle:IHandle;
+
+				[ 'ERROR', 'WARN', 'LOG', 'INFO', 'DEBUG' ].forEach(function (logLevel) {
+					var methodName = logLevel.toLowerCase();
+
+					handle = aspect.before(console, methodName, function () {
+						logMessage = arguments[0];
+					});
+
+					// TS7017
+					app.log(testMessage, (<any> LogLevel)[logLevel]);
+
+					assert.include(logMessage, testMessage,
+						'console.' + methodName + ' should be called with correct arguments');
+
+					handle.remove();
+					logMessage = '';
+				});
+			});
+		}
 	}
 });
