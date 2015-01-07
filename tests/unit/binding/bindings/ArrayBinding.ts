07a51e0d90a4c25a333ba13513f18cc9fc5d89db
Mangala SSS Khalsa <mkhalsa@sitepen.com>
2015-01-06 21:20:09 -0700

    Add unit tests: * binding/bindings/ArrayBinding * binding/bindings/DomInputBinding * binding/bindings/Es5Binding

---
+++ tests/unit/binding/bindings/ArrayBinding.ts
@@ -0,0 +1,169 @@
+/// <reference path="../../../intern" />
+
+import assert = require('intern/chai!assert');
+import bindingInterface = require('../../../../binding/interfaces');
+import ArrayBinding = require('../../../../binding/bindings/ArrayBinding');
+import registerSuite = require('intern!object');
+
+var collection:string[];
+var binding:ArrayBinding<string[]>;
+
+registerSuite({
+	name: 'mayhem/binding/bindings/ArrayBinding',
+
+	beforeEach() {
+		collection = [ 'foo', 'bar' ];
+	},
+
+	afterEach() {
+		binding && binding.destroy();
+		collection = null;
+	},
+
+	'.test'() {
+		assert.isTrue(ArrayBinding.test({ object: collection, path: '*', binder: null }));
+		assert.isFalse(ArrayBinding.test({ object: {}, path: '*', binder: null }));
+		assert.isFalse(ArrayBinding.test({ object: 'not an array', path: '*', binder: null }));
+		assert.isFalse(ArrayBinding.test({ object: collection, path: 'length', binder: null }));
+		assert.isFalse(ArrayBinding.test({ object: collection, path: 'value', binder: null }));
+	},
+
+	'#getObject'() {
+		binding = new ArrayBinding({ object: collection, path: '*', binder: null });
+		assert.strictEqual(binding.getObject(), collection);
+	},
+
+	pop() {
+		var dfd = this.async(500);
+
+		binding = new ArrayBinding({ object: collection, path: '*', binder: null });
+		binding.observe(dfd.callback(function (change:bindingInterface.IChangeRecord<number>):void {
+			assert.strictEqual(change.index, 1);
+			assert.sameMembers(change.removed, [ 'bar' ]);
+			assert.isUndefined(change.added);
+		}));
+		collection.pop();
+	},
+
+	push() {
+		var dfd = this.async(500);
+
+		binding = new ArrayBinding({ object: collection, path: '*', binder: null });
+		binding.observe(dfd.callback(function (change:bindingInterface.IChangeRecord<number>):void {
+			assert.strictEqual(change.index, 2);
+			assert.sameMembers(change.added, [ 'baz' ]);
+			assert.isUndefined(change.removed);
+		}));
+		collection.push('baz');
+	},
+
+	reverse() {
+		var dfd = this.async(500);
+
+		binding = new ArrayBinding({ object: collection, path: '*', binder: null });
+		binding.observe(dfd.callback(function (change:bindingInterface.IChangeRecord<number>):void {
+			assert.strictEqual(change.index, 0);
+			assert.sameMembers(change.added, [ 'bar', 'foo' ]);
+			assert.sameMembers(change.removed, [ 'foo', 'bar' ]);
+		}));
+		collection.reverse();
+	},
+
+	shift() {
+		var dfd = this.async(500);
+
+		binding = new ArrayBinding({ object: collection, path: '*', binder: null });
+		binding.observe(dfd.callback(function (change:bindingInterface.IChangeRecord<number>):void {
+			assert.strictEqual(change.index, 0);
+			assert.isUndefined(change.added);
+			assert.sameMembers(change.removed, [ 'foo' ]);
+		}));
+		collection.shift();
+	},
+
+	sort() {
+		var dfd = this.async(500);
+
+		binding = new ArrayBinding({ object: collection, path: '*', binder: null });
+		binding.observe(dfd.callback(function (change:bindingInterface.IChangeRecord<number>):void {
+			assert.strictEqual(change.index, 0);
+			assert.sameMembers(change.added, [ 'bar', 'foo' ]);
+			assert.sameMembers(change.removed, [ 'foo', 'bar' ]);
+		}));
+		collection.sort();
+	},
+
+	splice: {
+		beginning() {
+			var dfd = this.async(500);
+
+			binding = new ArrayBinding({ object: collection, path: '*', binder: null });
+			binding.observe(dfd.callback(function (change:bindingInterface.IChangeRecord<number>):void {
+				assert.strictEqual(change.index, 0);
+				assert.sameMembers(change.added, [ 'baz' ]);
+				assert.lengthOf(change.removed, 0);
+			}));
+			collection.splice(0, 0, 'baz');
+		},
+
+		'beginning with replacement'() {
+			var dfd = this.async(500);
+
+			binding = new ArrayBinding({ object: collection, path: '*', binder: null });
+			binding.observe(dfd.callback(function (change:bindingInterface.IChangeRecord<number>):void {
+				assert.strictEqual(change.index, 0);
+				assert.sameMembers(change.added, [ 'baz', 'xyz' ]);
+				assert.sameMembers(change.removed, [ 'foo' ]);
+			}));
+			collection.splice(0, 1, 'baz', 'xyz');
+		},
+
+		middle() {
+			var dfd = this.async(500);
+
+			binding = new ArrayBinding({ object: collection, path: '*', binder: null });
+			binding.observe(dfd.callback(function (change:bindingInterface.IChangeRecord<number>):void {
+				assert.strictEqual(change.index, 1);
+				assert.sameMembers(change.added, [ 'baz' ]);
+				assert.lengthOf(change.removed, 0);
+			}));
+			collection.splice(1, 0, 'baz');
+		},
+
+		'middle with replacement'() {
+			var dfd = this.async(500);
+
+			binding = new ArrayBinding({ object: collection, path: '*', binder: null });
+			binding.observe(dfd.callback(function (change:bindingInterface.IChangeRecord<number>):void {
+				assert.strictEqual(change.index, 1);
+				assert.sameMembers(change.added, [ 'baz', 'xyz' ]);
+				assert.sameMembers(change.removed, [ 'bar' ]);
+			}));
+			collection.splice(1, 1, 'baz', 'xyz');
+		},
+
+		end() {
+			var dfd = this.async(500);
+
+			binding = new ArrayBinding({ object: collection, path: '*', binder: null });
+			binding.observe(dfd.callback(function (change:bindingInterface.IChangeRecord<number>):void {
+				assert.strictEqual(change.index, 2);
+				assert.sameMembers(change.added, [ 'baz' ]);
+				assert.lengthOf(change.removed, 0);
+			}));
+			collection.splice(2, 0, 'baz');
+		}
+	},
+
+	unshift() {
+		var dfd = this.async(500);
+
+		binding = new ArrayBinding({ object: collection, path: '*', binder: null });
+		binding.observe(dfd.callback(function (change:bindingInterface.IChangeRecord<number>):void {
+			assert.strictEqual(change.index, 0);
+			assert.sameMembers(change.added, [ 'baz' ]);
+			assert.isUndefined(change.removed);
+		}));
+		collection.unshift('baz');
+	}
+});
---
+++ tests/unit/binding/bindings/Es5Binding.ts
@@ -0,0 +1,65 @@
+/// <reference path="../../../intern" />
+
+import assert = require('intern/chai!assert');
+import bindingInterface = require('../../../../binding/interfaces');
+import Es5Binding = require('../../../../binding/bindings/Es5Binding');
+import has = require('../../../../has');
+import registerSuite = require('intern!object');
+
+var binding:Es5Binding<any>;
+
+if (has('es5')) {
+	registerSuite({
+		name: 'mayhem/binding/bindings/Es5Binding',
+
+		'.test'() {
+			var obj:Object;
+
+			obj = {};
+			assert.isTrue(Es5Binding.test({ object: obj, path: 'foo', binder: null }));
+
+			Object.defineProperty(obj, 'foo', {
+				configurable: true,
+				enumerable: true,
+				writeable: true,
+				set: function (value) {
+					this.value = value;
+				}
+			});
+			assert.isTrue(Es5Binding.test({ object: obj, path: 'foo', binder: null }));
+
+			obj = {};
+			Object.defineProperty(obj, 'foo', {
+				configurable: true,
+				enumerable: true,
+				writeable: true,
+				value: ''
+			});
+			assert.isTrue(Es5Binding.test({ object: obj, path: 'foo', binder: null }));
+
+			obj = {};
+			Object.preventExtensions(obj);
+			assert.isFalse(Es5Binding.test({ object: obj, path: 'foo', binder: null }));
+
+			obj = {};
+			Object.defineProperty(obj, 'foo', {
+				configurable: false,
+				enumerable: true,
+				writeable: true
+			});
+			assert.isFalse(Es5Binding.test({ object: obj, path: 'foo', binder: null }));
+		},
+
+		binding() {
+			var dfd = this.async(500);
+			var obj:any = {};
+			var binding = new Es5Binding({ object: obj, path: 'foo', binder: null });
+
+			binding.observe(dfd.callback(function (change:bindingInterface.IChangeRecord<string>) {
+				assert.strictEqual(change.value, 'bar');
+				assert.isUndefined(change.oldValue);
+			}));
+			obj.foo = 'bar';
+		}
+	});
+}
