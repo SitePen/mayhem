2fc55dba7bedf9097324287f130978892acb7166
Colin Snover <github.com@zetafleet.com>
2015-02-06 07:52:16 +0000

    Fixes to release script

--- support/bower-release.sh
+++ support/bower-release.sh
@@ -2,11 +2,12 @@
 set -e
 
 rm -rf dist
-git clone --branch bower . dist
+git clone -n --branch bower . dist
 grunt build
+cp bower.json LICENSE package.json README.md dist
 cd dist
 git add -A .
-git commit -m "Update release"
+git commit -m "Update bower release"
 git push
 cd ..
 git push origin bower
