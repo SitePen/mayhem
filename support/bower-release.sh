d20dca44fd8e9c4216b1733bd6db1832390705bb
Colin Snover <github.com@zetafleet.com>
2015-02-11 08:09:09 +0000

    Update temporary bower release script with commit IDs

--- support/bower-release.sh
+++ support/bower-release.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
+COMMIT=$(git rev-parse HEAD)
+
 rm -rf dist
 git clone -n --branch bower . dist
 grunt build
-cp bower.json LICENSE package.json README.md dist
 cd dist
 git add -A .
-git commit -m "Update bower release"
+git commit -m "Update bower release from commit $COMMIT"
 git push
 cd ..
 git push origin bower
